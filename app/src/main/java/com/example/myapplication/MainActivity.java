042a4ccff1d60eb4ec601003eabf92f9b56d0a95
lijinbang777 <jacklee18364166614@gmail.com>
2022-07-24 23:42:22 +0800

    线性布局及button页面activity跳转

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("ios","viewdidload");
+        //Log分为Verbose,info,debug,warning,error信息
+        Log.d("ios","相当于iOS的viewdidload");
+
+       Button btn = findViewById(R.id.button_click);
+
+       //接口方法I
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent tent = new Intent(MainActivity.this,TestActivity.class);
+               startActivity(tent);
+           }
+       });
+
     }
 
+    
 
 }
